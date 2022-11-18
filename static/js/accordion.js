ffde7e463f8e903a83d691c85abc845db4562c43
Andrea <zorzi.andrea@outlook.com>
2022-11-18 20:24:06 +0100

    Improved homepage, added in focus states

--- static/js/accordion.js
+++ static/js/accordion.js
@@ -3,23 +3,84 @@ var i;
 
 
 for (i = 0; i < acc.length; i++) {
+
+    acc[i].addEventListener("mouseover", function () {
+        var image = this.children[0]
+
+        anime({
+            duration: 750,
+            targets: image,
+            rotateZ: 90,
+        }).play()
+
+    })
+
+
+    acc[i].addEventListener("mouseout", function () {
+        var image = this.children[0]
+
+        anime({
+            targets: image,
+            duration: 750,
+            rotateZ: 0,
+        }).play()
+
+
+    })
+
     acc[i].addEventListener("click", function () {
+
+        // Disable every active accordion
         for (i = 0; i < acc.length; i++) {
+            var acc_panels = acc[i].nextElementSibling;
+            var all_span_2 = acc[i].children[0].children[1]
+
             if (acc[i].classList.contains("active")) {
-                var acc_panel = acc[i].nextElementSibling;
-                
+
                 if (!(acc[i] == this)) {
-                    acc_panel.style.maxHeight = null;
+                    
+                    // animate all closings
+                    anime({
+                        duration: 1000,
+                        targets: all_span_2,
+                        left: [{ value: 30 + "%"}],
+                        right: [{value: 30 + "%"}],
+                    }).play()
+
+                    acc_panels.style.maxHeight = null;
                     acc[i].classList.remove("active")
                 }
             }
         }
+
         
+        // Toggle current panel 
         var panel = this.nextElementSibling;
+        var span_2 = this.children[0].children[1]
+
         this.classList.toggle("active");
+
         if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
+            
+            // Close panel
+            anime({
+                duration: 750,
+                targets: span_2,
+                left: [{ value: 30 + "%"}],
+                right: [{value: 30 + "%"}],
+            }).play()
+
+            panel.style.maxHeight = null;
         } else {
+
+            // Open panel
+            anime({
+                duration: 750,
+                targets: span_2,
+                left: [{ value: 50 + "%" }],
+                right: [{value: 50 + "%" }],
+            }).play()
+
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
   });
