3d5c9465197cc7ad00505509a52332d55e5affc9
Andrea <zorzi.andrea@outlook.com>
2022-12-02 09:54:28 +0100

    Features section fully functional

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
+
+from django.contrib.auth.decorators import login_required
+
 from .views import TrackerPageView, TrackerAddFavourite, TrackerRemoveFavourite
+
 
 urlpatterns = [
     path("", TrackerPageView.as_view(), name="tracker"),
-    path("add/<str:city>", TrackerAddFavourite.as_view(), name="add_to_favourites"),
+    path(
+        "add/<str:city>",
+        login_required(TrackerAddFavourite.as_view()),
+        name="add_to_favourites",
+    ),
     path(
         "remove/<str:city>",
-        TrackerRemoveFavourite,
+        login_required(TrackerRemoveFavourite.as_view()),
         name="remove_to_favourites",
     ),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from django.contrib.auth.decorators import login_required
 
 from .forms import SearchCityForm
 
@@ -11,6 +10,19 @@ from api.get_3hrs_forecast import get_3hrs_temperature_forecast
 
 # Create your views here.
 class TrackerPageView(View):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            favourites = dict()
+
+            for i in FavouriteCity.objects.filter(favourite=request.user).all():
+                favourites[i] = get_current_weather(i)
+
+            kwargs["favourites"] = favourites
+        else:
+            kwargs["favourites"] = None
+
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request, *args, **kwargs):
         default_city_current_weather = get_current_weather("Zurich")
         default_city_forecast_weather = get_3hrs_temperature_forecast("Zurich")
@@ -19,15 +31,8 @@ class TrackerPageView(View):
             "current_weather": default_city_current_weather,
             "search_city_form": SearchCityForm,
             "city_forecast": default_city_forecast_weather,
+            "favourites": kwargs["favourites"],
         }
-
-        if request.user.is_authenticated:
-            favourites = dict()
-
-            for i in FavouriteCity.objects.filter(favourite=request.user).all():
-                favourites[i] = get_current_weather(i)
-
-            context["favourites"] = favourites
 
         return render(request, "tracker.html", context)
 
@@ -37,12 +42,14 @@ class TrackerPageView(View):
             city = form.cleaned_data["city"]
 
             city_current_weather = get_current_weather(city)
+
             city_forecast = get_3hrs_temperature_forecast(city)
 
             context = {
                 "city_forecast": city_forecast,
                 "current_weather": city_current_weather,
                 "search_city_form": SearchCityForm,
+                "favourites": kwargs["favourites"],
             }
 
             return render(request, "tracker.html", context)
@@ -56,13 +63,16 @@ class TrackerAddFavourite(View):
         city = kwargs["city"]
         if not FavouriteCity.objects.filter(city=city).exists():
             FavouriteCity(city=city).save()
-            FavouriteCity.objects.get(city=city).favourite.add(request.user)
+
+        FavouriteCity.objects.get(city=city).favourite.add(request.user)
 
         return redirect("tracker")
 
 
-@login_required
 class TrackerRemoveFavourite(View):
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
     def get(self, request, *args, **kwargs):
         city = kwargs["city"]
         FavouriteCity.objects.get(city=city).favourite.remove(request.user)
