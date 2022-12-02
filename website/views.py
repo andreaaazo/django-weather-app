5c8c8517cc842784cbdda5333ff6961a90681bae
Andrea <zorzi.andrea@outlook.com>
2022-12-02 19:10:35 +0100

    Added home card slider

--- website/views.py
+++ website/views.py
@@ -11,12 +11,18 @@ class HomePageView(View):
         new_york = get_current_weather("New York")
         london = get_current_weather("London")
         tokyo = get_current_weather("Tokyo")
+        los_angeles = get_current_weather("Los Angeles")
+        alaska = get_current_weather("Alaska")
+        rio_de_janeiro = get_current_weather("Rio de Janeiro")
 
         context = {
             "dubai": dubai,
             "new_york": new_york,
             "london": london,
             "tokyo": tokyo,
+            "los_angeles": los_angeles,
+            "alaska": alaska,
+            "rio_de_janeiro": rio_de_janeiro,
         }
 
         return render(request, "home.html", context)
