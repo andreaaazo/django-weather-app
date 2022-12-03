8f727f5ba4c5c83ca501816de72d6be97448a7b8
Andrea <zorzi.andrea@outlook.com>
2022-12-03 18:50:32 +0100

    Homepage fixes

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,13 +1,10 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 
 class SearchCityForm(forms.Form):
     city = forms.CharField(
         max_length=30,
         label="",
-        widget=forms.TextInput(
-            attrs={
-                "placeholder": "London"
-            }
-        )
+        widget=forms.TextInput(attrs={"placeholder": _("London")}),
     )
