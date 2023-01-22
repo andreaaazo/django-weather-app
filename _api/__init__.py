576680ed4948b7853a0ae9d3bc9efa98688c7be6
Andrea <zorzi.andrea@outlook.com>
2023-01-22 22:09:43 +0100

    Bug: api module not imported correctly

--- _api/__init__.py
+++ _api/__init__.py
@@ -1,1 +1,1 @@
-
+from . import prevision
--- website/views.py
+++ website/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.views import View
 
-from _api import prevision
+from _api.prevision import get_current_weather, get_3hrs_temperature_forecast
 
 # Create your views here.
 class HomePageView(View):
     def get(self, request, *args, **kwargs):
 
-        dubai = prevision.get_current_weather("Dubai")
-        new_york = prevision.get_current_weather("New York")
-        london = prevision.get_current_weather("London")
-        tokyo = prevision.get_current_weather("Tokyo")
-        los_angeles = prevision.get_current_weather("Los Angeles")
-        alaska = prevision.get_current_weather("Alaska")
-        rio_de_janeiro = prevision.get_current_weather("Rio de Janeiro")
+        dubai = get_current_weather("Dubai")
+        new_york = get_current_weather("New York")
+        london = get_current_weather("London")
+        tokyo = get_current_weather("Tokyo")
+        los_angeles = get_current_weather("Los Angeles")
+        alaska = get_current_weather("Alaska")
+        rio_de_janeiro = get_current_weather("Rio de Janeiro")
 
         context = {
             "dubai": dubai,
